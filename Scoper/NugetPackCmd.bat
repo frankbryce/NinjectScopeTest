e4684d73a035963886edd1db45a531432d9c8cc4
John Carpenter <jonnyjack7@gmail.com>
2016-02-24 17:36:24 -0500

    updated nuspec file and added Autofac as a dependency to the project

--- Scoper/NugetPackCmd.bat
+++ Scoper/NugetPackCmd.bat
@@ -1,2 +1,2 @@
 echo "MAKE SURE THAT YOU HAVE JUST BUILT THE CODE IN RELEASE MODE AND EXECUTED THE UNIT TESTS!"
-cmd /c ..\.nuget\nuget.exe pack NinjectScopeTest.csproj -Prop Configuration=Release
+cmd /c ..\.nuget\nuget.exe pack Scoper.csproj -Prop Configuration=Release
