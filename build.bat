1d0baee81a7ac320c7563bc1c8898cd2dde918ad
John Carpenter <jonnyjack7@gmail.com>
2016-03-30 15:29:42 -0400

    Updating .nuspec files and the build file to create the nuget packages that I had before.

--- build.bat
+++ build.bat
@@ -3,8 +3,8 @@ set config=%1
 if "%config%" == "" (
    set config=Release
 )
- 
-set version=1.0.0
+
+set version=0.5.4
 if not "%PackageVersion%" == "" (
    set version=%PackageVersion%
 )
@@ -21,3 +21,5 @@ mkdir Build\lib
 mkdir Build\lib\net45
 
 %nuget% pack "Scoper\Scoper.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+%nuget% pack "Scoper.Ninject\Scoper.Ninject.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+%nuget% pack "Scoper.Autofac\Scoper.Autofac.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
